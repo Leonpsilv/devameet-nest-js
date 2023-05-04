60df7d5ce3f20fd69a18bed116c1ea89181d0bd6
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-05-04 18:24:42 -0400

    login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,7 @@
+export const MessagesHelper = {
+  AUTH_LOGIN_NOT_FOUND: 'Login informado não é válido.',
+
+  AUTH_PASSWORD_NOT_FOUND: 'Senha informada não é válida.',
+
+  AUTH_PASSWORD_OR_EMAIL_NOT_FOUND: 'E-mail e/ou senha inválidos!',
+};
