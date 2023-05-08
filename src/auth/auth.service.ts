7cf64b75a7e75d0c44252c42d5eb9b50656f5776
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-05-08 11:47:08 -0400

    register endpoint

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,18 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
 import { MessagesHelper } from './helpers/messages.helper';
+import { RegisterDto } from 'src/user/dtos/register.dto';
+import { UserService } from 'src/user/services/user.service';
+import { UserMessagesHelper } from 'src/user/helpers/messages.helper';
 
 @Injectable()
 export class AuthService {
+  private logger = new Logger(AuthService.name);
+
+  constructor(private readonly userService: UserService) {}
+
   login(dto: LoginDto) {
+    this.logger.debug('login - started');
     if (dto.login !== 'teste@teste.com' || dto.password !== 'senha123') {
       throw new BadRequestException(
         MessagesHelper.AUTH_PASSWORD_OR_EMAIL_NOT_FOUND,
@@ -12,5 +20,18 @@ export class AuthService {
     }
 
     return dto;
+  }
+
+  async register(dto: RegisterDto) {
+    this.logger.debug('register - started');
+
+    if (await this.userService.existsByEmail(dto.email)) {
+      this.logger.debug(`register - same email found for: ${dto.email}`);
+      throw new BadRequestException(UserMessagesHelper.REGISTER_EMAIL_FOUND);
+    }
+
+    const user = await this.userService.create(dto);
+
+    return user;
   }
 }
---
+++ src/user/dtos/register.dto.ts
@@ -0,0 +1,28 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { UserMessagesHelper } from '../helpers/messages.helper';
+
+export class RegisterDto {
+  @MinLength(2, { message: UserMessagesHelper.REGISTER_NAME_NOT_FOUND })
+  name: string;
+
+  @IsEmail({}, { message: UserMessagesHelper.REGISTER_EMAIL_NOT_VALID })
+  email: string;
+
+  @IsNotEmpty({ message: UserMessagesHelper.AUTH_PASSWORD_NOT_FOUND })
+  @MinLength(4, { message: UserMessagesHelper.REGISTER_STRONG_PASSWORD })
+  @MaxLength(20, { message: UserMessagesHelper.REGISTER_STRONG_PASSWORD })
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: UserMessagesHelper.REGISTER_STRONG_PASSWORD,
+  })
+  password: string;
+
+  @IsString()
+  avatar: string;
+}
